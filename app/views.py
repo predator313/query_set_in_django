8da274ca6a9394d2716a60e4945ab16f2b1fb34a
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-04 18:57:07 +0530

    displaying the objects in home page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
+#as we know in python each object is equivalenet to the each row
+#of the db table
+#so we use objects.all method to fetch all the record of the data
 def fun(request):
-    return render(request,'app/home.html')
+    student_data=Student.objects.all()
+    return render(request,'app/home.html',{'student_data':student_data})
