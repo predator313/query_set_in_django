64c19c51fdeabe96ad5f2be1e171c78c9c757442
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-04 15:50:00 +0530

    making student model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=40)
+    roll_number=models.IntegerField(unique=True,null=False)
+    address=models.CharField(max_length=60)
+    marks=models.IntegerField()
+    res_date=models.DateField()
